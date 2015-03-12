6362e1b0dbe8541ebe89b2d8b90fb32f530ddb5b
Rhys Bower <bowerrhys@gmail.com>
2015-03-12 10:07:47 -0700

    Create my theme.

--- wp-content/themes/rhysbower/js/customize-preview.js
+++ wp-content/themes/rhysbower/js/customize-preview.js
@@ -3,12 +3,12 @@
  */
 
 ( function( $ ) {
-	var $style = $( '#twentyfifteen-color-scheme-css' ),
+	var $style = $( '#rhysbower-color-scheme-css' ),
 		api = wp.customize;
 
 	if ( ! $style.length ) {
-		$style = $( 'head' ).append( '<style type="text/css" id="twentyfifteen-color-scheme-css" />' )
-		                    .find( '#twentyfifteen-color-scheme-css' );
+		$style = $( 'head' ).append( '<style type="text/css" id="rhysbower-color-scheme-css" />' )
+		                    .find( '#rhysbower-color-scheme-css' );
 	}
 
 	// Site title.
