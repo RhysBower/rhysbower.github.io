1672074f427c5928f86e8baff03c6c02592054f4
Rhys Bower <bowerrhys@gmail.com>
2015-03-12 23:51:40 -0700

    Move blog to own folder and begin to convert site from Ruby to PHP.

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Rhys Bower</title>
+  <title><?= $title ?></title>
 
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="/static/bootstrap/css/bootstrap.min.css">
@@ -23,7 +23,7 @@
   <![endif]-->
 </head>
 <body id="top">
-  <% if @page == "/" %>
+  <?php if($page == "/") { ?>
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -47,7 +47,7 @@
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
-  <% else %>
+  <?php } else { ?>
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -69,56 +69,4 @@
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
-  <% end %>
-
-  <%= yield %>
-
-  <footer>
-    <div class="footer-info">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-4">
-            Richmond BC, Canada
-            <img alt="" class="map img-responsive" data-user="3077743" height="230" src="/img/richmond.png" width="230">
-          </div>
-          <div class="col-sm-4">
-            <div class="row">
-              My Social Sites
-            </div>
-            <div class="row">
-              <a href="https://github.com/RhysBower" target="_blank" class="link">
-                <i class="fa fa-github-square social-icon"></i>
-              </a>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            Email
-            <div class="row">
-              <a href="mailto:bowerrhys@gmail.com?Subject=Website%20Inquiry" class="link">
-                <i class="fa fa-envelope-o"></i> bowerrhys@gmail.com
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            Copyright © Rhys Bower 2015
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="/js/jquery-1.11.2.min.js"></script>
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="/bootstrap/js/bootstrap.min.js"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-  <script src="/js/jqBootstrapValidation.js"></script>
-  <script src="/js/main.js"></script>
-</body>
-</html>
+  <?php } ?>
