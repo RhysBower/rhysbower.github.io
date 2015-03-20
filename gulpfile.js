6fa4e6c0bfdede5afe5a504bf9eb075c76a72899
Rhys Bower <bowerrhys@gmail.com>
2015-03-20 09:22:21 -0700

    Initial commit of gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var duration = require('gulp-duration')
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass', function () {
+    gulp.src('./app/_sass/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./app/css/main.css'));
 });
