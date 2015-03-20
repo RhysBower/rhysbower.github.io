9f11085c474183f4f9280856ea5643ae9a136019
Rhys Bower <bowerrhys@gmail.com>
2015-03-20 09:45:19 -0700

    Update permalinks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var duration = require('gulp-duration')
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('./app/_sass/main.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./app/css/main.css'));
+    gulp.src('./_/app/css/main.scss')
+        .pipe(sass({ includePaths : ['_/app/_sass/'] }))
+        .pipe(gulp.dest('css'));
 });
