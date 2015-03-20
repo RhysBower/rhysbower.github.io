457dee8dd6150070d9307a2092a2223a9eaca42b
Rhys Bower <bowerrhys@gmail.com>
2015-03-20 13:28:47 -0700

    Optimize images.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,13 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var csscomb = require('gulp-csscomb');
+var csslint = require('gulp-csslint');
+var jslint = require('gulp-jslint');
+
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 var config = {
      sassPath: './app/_sass',
@@ -14,6 +21,13 @@ var config = {
 gulp.task('css', function() {
   return gulp.src('./app/_css/main.scss')
     .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(csscomb())
+    .pipe(csslint())
+    .pipe(csslint.reporter())
     .pipe(minifyCSS())
     .pipe(gulp.dest('./app/css'));
 });
@@ -30,8 +44,30 @@ gulp.task('js', function() {
                    './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
                    './app/_js/main.js'])
     .pipe(concat('all.js'))
+    .pipe(jslint({
+            node: true,
+            evil: true,
+            nomen: true,
+            global: [],
+            predef: [],
+            reporter: 'default',
+            errorsOnly: false
+        }))
+        .on('error', function (error) {
+            console.error(String(error));
+        })
     .pipe(uglify())
     .pipe(gulp.dest('./app/js/'));
+});
+
+gulp.task('img', function () {
+    return gulp.src('app/_img/**')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('app/img'));
 });
 
 gulp.task('default', ['font-awesome', 'css', 'js'])
