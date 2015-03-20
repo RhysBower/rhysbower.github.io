ad6c6b29cbee28af0ab1e13fd7d8158d827d4a62
Rhys Bower <bowerrhys@gmail.com>
2015-03-20 12:45:20 -0700

    Compile CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,28 @@ var duration = require('gulp-duration')
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 
 var config = {
      sassPath: './app/_sass',
      bowerDir: './bower_components' 
 }
 
-gulp.task('css', function() { 
-    return gulp.src('./app/css/main.scss')
-         .pipe(sass() 
-          .on("error", notify.onError(function (error) {
-              return "Error: " + error.message;
-            }))) 
-         .pipe(gulp.dest('./app/css')); 
+gulp.task('css', function() {
+  return gulp.src('./app/_css/main.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./app/css'));
 });
 
+gulp.task('font-awesome', function() {
+  return gulp.src('./bower_components/fontawesome/fonts/*.*')
+    .pipe(gulp.dest('./app/fonts'));
+});
+
+
 gulp.task('js', function() {
-  return gulp.src(['./bower_components/dist/js/jquery.min.js',
+  return gulp.src(['./bower_components/jquery/dist/jquery.min.js',
                    './bower_components/jquery_easing/jquery_easing.js',
                    './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
                    './app/_js/main.js'])
@@ -29,4 +34,4 @@ gulp.task('js', function() {
     .pipe(gulp.dest('./app/js/'));
 });
 
-gulp.task('default', ['js'])
+gulp.task('default', ['font-awesome', 'css', 'js'])
