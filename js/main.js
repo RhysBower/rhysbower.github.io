e0ac51cb3a917aae3524a913b5f16b284e178f68
Rhys Bower <bowerrhys@gmail.com>
2015-03-19 22:17:41 -0700

    Update game layout.

--- js/main.js
+++ js/main.js
@@ -26,37 +26,6 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation({
-
-  submitSuccess: function ($form, event) {
-
-    $("#submit").attr("disabled", "disabled");
-
-    $.ajax({
-      type: 'POST',
-      url: $form.attr('action'),
-      data: $form.serialize(),
-      success: function(data)
-      { 
-        $("#submit").removeAttr("disabled");
-
-        if(data == "true") {
-          $('#contactForm').trigger("reset");
-          $('#alert_placeholder').html('<div class="alert alert-dismissible alert-success"><a class="close" data-dismiss="alert">×</a><span>Your message has been sent. I\'ll respond to it soon.</span></div>')
-        }else{
-          $('#alert_placeholder').html('<div class="alert alert-dismissible alert-danger"><a class="close" data-dismiss="alert">×</a><span>Something went wrong. Please try again.</span></div>')
-        }
-      }
-    });
-
-    // will not trigger the default submission in favor of the ajax function
-    event.preventDefault();
-  }
-
-});
-
-} );
-
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
