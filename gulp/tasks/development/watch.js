dcb39933fe1f378df6cd3a7ec23fc42de81f8a97
Rhys Bower <bowerrhys@gmail.com>
2015-03-25 17:58:32 -0700

    Update Jekyll watch file extensions.

--- gulp/tasks/development/watch.js
+++ gulp/tasks/development/watch.js
@@ -4,5 +4,5 @@ gulp.task('watch', ['browser-sync'], function() {
     gulp.watch('./app/_css/*.scss', ['sass', browserSync.reload]);
     gulp.watch('./app/_js/*.js', ['js', browserSync.reload]);
     gulp.watch('./app/_img/*.*', ['img', browserSync.reload]);
-    gulp.watch('app/**/*.*', ['jekyll', browserSync.reload]);
+    gulp.watch('app/**/*.{html,markdown,md,xml}', ['jekyll', browserSync.reload]);
 });
