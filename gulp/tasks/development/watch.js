af5cab20bb00f1ff14a131b2fd5685f6a5a56e57
Rhys Bower <bowerrhys@gmail.com>
2015-04-01 12:19:23 -0700

    Fix gulp img watch task.

--- gulp/tasks/development/watch.js
+++ gulp/tasks/development/watch.js
@@ -4,6 +4,6 @@ var browserSync = require('browser-sync');
 gulp.task('watch', ['browser-sync'], function() {
     gulp.watch('./app/_css/*.scss', ['sass', browserSync.reload]);
     gulp.watch('./app/_js/*.js', ['js', browserSync.reload]);
-    gulp.watch('./app/_img/*.*', ['img', browserSync.reload]);
+    gulp.watch('./app/_img/**/*.*', ['img', browserSync.reload]);
     gulp.watch('app/**/*.{html,markdown,md,xml}', ['jekyll', browserSync.reload]);
 });
